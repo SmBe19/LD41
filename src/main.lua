9e44f5650d56a6b175d14930ac752f359fa196b1
Benjamin Schmid <git@schmidb.ch>
2018-04-22 08:51:10 +0200

    move asset folder

--- src/main.lua
+++ src/main.lua
@@ -2,6 +2,21 @@
 -- Main entry point for game
 --
 
+imgs = {}
+consts = {}
+
+function love.load()
+    imgs.bar = love.graphics.newImage("assets/bar.png")
+
+    consts.width = love.graphics.getWidth()
+    consts.height = love.graphics.getHeight()
+    consts.sx = 2
+    consts.sy = 2
+end
+
+function love.update()
+end
+
 function love.draw()
-    love.graphics.print("hello world", 200, 100)
+    love.graphics.draw(imgs.bar, 0, 0, 0, consts.sx, consts.sy)
 end
