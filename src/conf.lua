34fc9b4ed40ba67ac94c8da05f1f123895509a0d
Benjamin Schmid <git@schmidb.ch>
2018-04-22 08:11:58 +0200

    Initial Setup

---
+++ src/conf.lua
@@ -0,0 +1,5 @@
+function love.conf(t)
+    t.window.title = "Shot Shot"
+    t.window.width = 640
+    t.window.height = 480
+end
---
+++ src/main.lua
@@ -0,0 +1,7 @@
+--
+-- Main entry point for game
+--
+
+function love.draw()
+    love.graphics.print("hello world", 200, 100)
+end
